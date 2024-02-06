3ea3fc538595cd9f9ef391c1890b9fabf91721fd
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-06 18:31:37 +0530

    js basics

---
+++ basic.js
@@ -0,0 +1,30 @@
+console.log("Welcome to javascript");
+let a=10;
+let b=5;
+
+console.log(a+b);
+
+a=5;
+console.log(typeof(a));
+
+let c="Java Script"
+console.log(typeof(c))
+console.log(a +" "+c)
+console.log(Number.MAX_VALUE)
+
+let bool=4>5;
+console.log(bool);
+
+let d=null;
+console.log(typeof(d));
+
+console.log(Boolean(4))
+console.log(Boolean(0))
+
+console.log(Math.pow(6,2))
+
+console.log(2**4)
+
+h="AbC"
+i="sc"
+console.log(h>i)
