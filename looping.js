3c29cf77f2d4a9f027251f3783ef9c8db3ab8944
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-07 10:38:02 +0530

    Create looping.js

---
+++ looping.js
@@ -0,0 +1,19 @@
+let a=5;
+for(i=0;i<=a;i++){
+    console.log(i)
+}
+
+console.log('-----------------')
+while(a%2!=0){
+    console.log(a+=1)
+   
+}
+
+console.log('-----------------')
+aa=10
+do
+{
+    console.log("Hi",aa)
+    a++
+}
+while(aa<=5)
