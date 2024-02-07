fe3353cc903244cc17728a4314967dce5f8160f0
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-07 12:18:21 +0530

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,5 @@
 const prompt=require("prompt-sync")();
-let ageString = prompt("What's your age? ");
-let age = parseInt(ageString); 
+let age = prompt("What's your age? ");
 
 if (age >= 10 && age <= 20) {
     console.log(`Age ${age} lies in the range of 10 to 20`);
@@ -8,7 +7,8 @@ if (age >= 10 && age <= 20) {
     console.log("Age is out of range");
 }
 
-let s=8
+let ss=prompt("What's your Number? ");
+let s=parseInt(ss)
 switch(s){
     case 1:
     {
@@ -27,3 +27,33 @@ switch(s){
     }
     default : console.log("Stop")
 }
+
+
+let n=prompt('Enter the number ')
+
+if(n%2==0 && n%3==0){
+    console.log(`Number ${n} is divisible by both 2 and 3 `)
+}
+else{
+    console.log("Please enter the correct no")
+}
+
+if(n%2==0 || n%3==0){
+    console.log(`Number ${n} is divisible by both 2 or 3 `)
+}
+else{
+    console.log("Please enter the correct no")
+}
+
+let ages=prompt('Enter the Age ')
+if(ages>=18){
+    console.log("You can Drive")
+}
+else{
+    console.log("You can't Drive")
+}
+
+console.log("---------------------")
+age=20
+a=age>18? 'Can drive':'Canot drive'
+console.log(a)
