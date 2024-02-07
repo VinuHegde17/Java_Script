3b43ab5e6f913fd272209b1b78717ad7b3976f7e
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-07 11:07:06 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,26 @@
+let s="Javascript"
+console.log( res=s +" "+3)
+console.log(typeof(s))
+const item={
+    name:"abcde",
+    price :150
+}
+
+console.log(item)
+
+item['class']='A'
+console.log(item)
+
+const dict={
+    text1:"Meaning1",
+    text2:"Meaning2",
+    text3:"Meaning3",
+    text4:"Meaning4",
+    text5:"Meaning5"
+}
+
+console.log(dict)
+
+console.log(dict.text1)
+
+
