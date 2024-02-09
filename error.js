aaeb4df6be0b81f0b48ea9105c1f948ea7d81f34
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-09 15:32:31 +0530

    Create error.js

---
+++ error.js
@@ -0,0 +1,23 @@
+setTimeout(()=>{
+console.log("Hacking wifi password...............please wait...........")
+},1000)
+
+setTimeout(()=>{
+    console.log("Getting a values")
+},2000)
+
+setTimeout(()=>{
+console.log("Hacking wifi password...............please wait...........")
+},3000)
+
+setTimeout(()=>{
+    console.log("Getting a values")
+},4000)
+
+setTimeout(()=>{
+    console.log("Hacking wifi password...............please wait...........")
+},5000)
+    
+    setTimeout(()=>{
+        console.log("Getting a values")
+},6000)
