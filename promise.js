84027b9c1dcca3709b86c7e642bd711f0f5535ae
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-09 17:26:18 +0530

    .

---
+++ promise.js
@@ -0,0 +1,16 @@
+let promise=new Promise(function(resolve,reject){
+    alert("Hello")
+    resolve(56)
+});
+
+//let promise = new Promise(function(resolve, reject) {});
+
+
+console.log("Hello one")
+setTimeout(()=>{
+    console.log("Hacking wifi password...............please wait...........")
+    },1000)
+
+    console.log("My name is"+" Hello world")
+
+console.log(promise)
