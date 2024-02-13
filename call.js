2c0bd030706fce6b02cc976530ea072953666f70
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-13 15:17:06 +0530

    .

--- call.js
+++ call.js
@@ -5,3 +5,15 @@ function cal(a,b, sumcallback) {
  sumcallback(a,b)   
 }
 cal(4,6,sum)
+
+function greet(name, callback) {
+  setTimeout(function() {
+    callback("Hello, " + name);
+  }, 1000);
+}
+
+function displayGreeting(message) {
+  console.log(message);
+}
+
+greet("John", displayGreeting);
