0e2cf100168952fbeb4ba1911b5fa84e0bf0a1a9
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-16 11:46:57 +0530

    .

---
+++ jsasync.js
@@ -0,0 +1,16 @@
+function myDisplayer(message) {
+    console.log(message);
+  }
+function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Welcome");
+  }
+  
+  myFirst();
+  mySecond();
+
+
+  
