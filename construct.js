7b841f03e878b845846b74e0c581c487c00e787a
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-22 15:40:06 +0530

    Update construct.js

--- construct.js
+++ construct.js
@@ -7,10 +7,12 @@ function Father(first,last,age,bg) {
     this.bg=bg;
 }
 
-const myFather= new 
-
-
-Father("Abc","Xyz",50,"B+");
+const myFather= new Father("Abc","Xyz",50,"B+");
 console.log(myFather.First_name);
 myFather.height='165 cms'
 console.log(myFather.height);
+
+myFather.name=function (){
+    return this.Last_name +" "+ this.First_name
+}
+console.log(myFather.name());
