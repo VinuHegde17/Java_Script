bf4634ba92f2296f81d61421f1e87250e983dcf4
VinuHegde17 <vinayhegde0820@gmail.com>
2024-02-22 12:54:11 +0530

    Create construct.js

---
+++ construct.js
@@ -0,0 +1,16 @@
+const { log } = require("async");
+
+function Father(first,last,age,bg) {
+    this.First_name=first;
+    this.Last_name=last;
+    this.age=age;
+    this.bg=bg;
+}
+
+const myFather= new 
+
+
+Father("Abc","Xyz",50,"B+");
+console.log(myFather.First_name);
+myFather.height='165 cms'
+console.log(myFather.height);
